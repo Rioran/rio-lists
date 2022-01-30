540b725c79d2462e0319e88c85e5f0ec8eb2debf
Rioran <voronov_rv@mail.ru>
2022-01-30 19:08:10 +0300

    + item deactivation

--- services/web/riolists/__init__.py
+++ services/web/riolists/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config.from_object("riolists.config.Config")
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "PUT"])
 def main_page():
     if request.method == "GET":
         user = User.query.get(1)
@@ -20,5 +20,10 @@ def main_page():
         amount = request.form["amount"]
         item = Items(text=text, amount=amount)
         db.session.add(item)
-        db.session.commit()
-        return redirect(request.url)
+    if request.method == "PUT":
+        item_id = request.form["item_id"]
+        user_id = request.form["user_id"]
+        item = Items.query.filter_by(id=item_id, user_id=user_id).first()
+        item.is_active = False
+    db.session.commit()
+    return redirect(request.url)
